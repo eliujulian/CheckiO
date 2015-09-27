3121a4cec9b9cc8354dcefc613e03e0e4439d403
Eliu Schmitt <mail@eliu.de>
2015-09-27 19:38:15 +0200

    new mission

---
+++ Elementary/12_End_of_other.py
@@ -0,0 +1,14 @@
+def checkio(words_set):
+    for word in words_set:
+        for w in words_set:
+            if w.endswith(word) and w != word:
+                return True
+    return False
+
+
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
